aa742326f1aae0a34a7cacd9f83749a7ee3d82be
Levi O. Vasconcelos <leviovasconcelos@gmail.com>
2019-01-25 18:51:37 +0100

    removing affine parameters from batchnorm

--- src/models/DIALResNet.py
+++ src/models/DIALResNet.py
@@ -105,9 +105,9 @@ class ResNet(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal(m.weight, mode='fan_out')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant(m.weight, 1)
-                nn.init.constant(m.bias, 0)
+            #elif isinstance(m, nn.BatchNorm2d):
+                #nn.init.constant(m.weight, 1)
+                #nn.init.constant(m.bias, 0)
 
     def set_domain(self, source=True):
         for m in self.modules():
