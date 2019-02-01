ae971ee901c9516c204c419ebde892440aa7d5bf
Levi O. Vasconcelos <leviovasconcelos@gmail.com>
2019-02-01 19:03:00 +0100

    fixing split betwen source and target from fusion

--- src/train.py
+++ src/train.py
@@ -91,48 +91,57 @@ def dial_step(args, split, epoch, (loader, len_loader), model, optimizer = None,
     if split == 'train':
       optimizer.zero_grad()
     #(sourceInput, sourceLabel, sourceMeta), (targetInput, targetLabel, targetMeta) = data
-    sourceInput = torch.stack([x for i,x in enumerate(input) if abs(meta[i,0]) == 1])
-    sourceLabel = torch.stack([x for i,x in enumerate(target) if abs(meta[i,0]) == 1])
-    sourceMeta = torch.stack([x for i,x in enumerate(meta) if abs(meta[i,0]) == 1])
+    sourceInput, sourceLabel, sourceMeta = torch.Tensor(), torch.Tensor(), torch.Tensor()
+    targetInput, targetLabel, targetMeta = torch.Tensor(), torch.Tensor(), torch.Tensor()
+    source_output, target_output = torch.Tensor(), torch.Tensor()
+    source_loss_value, target_loss_value = 0, 0
     
-    targetInput = torch.stack([x for i,x in enumerate(input) if abs(meta[i,0]) > 1])
-    targetLabel = torch.stack([x for i,x in enumerate(target) if abs(meta[i,0]) > 1])
-    targetMeta = torch.stack([x for i,x in enumerate(meta) if abs(meta[i,0]) > 1])
+    source_input_list = [x for i,x in enumerate(input) if abs(meta[i,0]) == 1]
+    if (len(source_input_list) > 0): 
+          sourceInput = torch.stack(source_input_list)
+          sourceLabel = torch.stack([x for i,x in enumerate(target) if abs(meta[i,0]) == 1])
+          sourceMeta = torch.stack([x for i,x in enumerate(meta) if abs(meta[i,0]) == 1])
     
-    source_input_var = torch.autograd.Variable(sourceInput.cuda())
-    source_label_var = torch.autograd.Variable(sourceLabel)
-    model.set_domain(source=True)
-    source_output = model(source_input_var)
-    source_loss = ShapeConsistencyCriterion(nViews, supWeight = 1, unSupWeight = args.shapeWeight, M = M)(source_output.cpu(), source_label_var, torch.autograd.Variable(sourceMeta))
-    if split == 'train':
-          source_loss.backward()
-    source_loss_value = source_loss.data[0]
-    del source_loss
+    target_input_list = [x for i,x in enumerate(input) if abs(meta[i,0]) > 1]
+    if (len(target_input_list) > 0):
+          targetInput = torch.stack(target_input_list)
+          targetLabel = torch.stack([x for i,x in enumerate(target) if abs(meta[i,0]) > 1])
+          targetMeta = torch.stack([x for i,x in enumerate(meta) if abs(meta[i,0]) > 1])
     
-    target_input_var = torch.autograd.Variable(targetInput.cuda())
-    target_label_var = torch.autograd.Variable(targetLabel)
-    model.set_domain(source=False)
-    target_output = model(target_input_var)
-    target_loss = ShapeConsistencyCriterion(nViews, supWeight = 1, unSupWeight = args.shapeWeight, M = M)(target_output.cpu(), target_label_var, torch.autograd.Variable(targetMeta))
-    
-    if split == 'train':
-      target_loss.backward()
-    target_loss_value = target_loss.data[0]
-    del target_loss
+    if (len(source_input_list) > 0):
+          source_input_var = torch.autograd.Variable(sourceInput.cuda())
+          source_label_var = torch.autograd.Variable(sourceLabel)
+          model.set_domain(source=True)
+          source_output = model(source_input_var)
+          source_loss = ShapeConsistencyCriterion(nViews, supWeight = 1, unSupWeight = args.shapeWeight, M = M)(source_output.cpu(), source_label_var, torch.autograd.Variable(sourceMeta))
+          if split == 'train':
+                source_loss.backward()
+          source_loss_value = source_loss.data[0]
+          del source_loss
+    if (len(target_input_list) > 0):
+          target_input_var = torch.autograd.Variable(targetInput.cuda())
+          target_label_var = torch.autograd.Variable(targetLabel)
+          model.set_domain(source=False)
+          target_output = model(target_input_var)
+          target_loss = ShapeConsistencyCriterion(nViews, supWeight = 1, unSupWeight = args.shapeWeight, M = M)(target_output.cpu(), target_label_var, torch.autograd.Variable(targetMeta))
+          if split == 'train':
+                target_loss.backward()
+          target_loss_value = target_loss.data[0]
+          del target_loss
     if split == 'train':
       optimizer.step()
     
-    input = torch.cat((sourceInput, targetInput), 0)
-    target = torch.cat((sourceLabel, targetLabel), 0)
-    output = torch.cat((source_output, target_output), 0)
-    meta = torch.cat((sourceMeta, targetMeta), 0)
+    input_ = torch.cat((sourceInput, targetInput), 0)
+    target_ = torch.cat((sourceLabel, targetLabel), 0)
+    output_ = torch.cat((source_output, target_output), 0)
+    meta_ = torch.cat((sourceMeta, targetMeta), 0)
     if split == 'test':
-      for j in range(input.numpy().shape[0]):
-        img = (input.numpy()[j] * 255).transpose(1, 2, 0).astype(np.uint8)
-        cv2.imwrite('{}/img_{}/{}.png'.format(args.save_path, tag, i * input.numpy().shape[0] + j), img)
-        gt = target.cpu().numpy()[j]
-        pred = (output.data).cpu().numpy()[j]
-        vis = meta.cpu().numpy()[j][5:]
+      for j in range(input_.numpy().shape[0]):
+        img = (input_.numpy()[j] * 255).transpose(1, 2, 0).astype(np.uint8)
+        cv2.imwrite('{}/img_{}/{}.png'.format(args.save_path, tag, i * input_.numpy().shape[0] + j), img)
+        gt = target_.cpu().numpy()[j]
+        pred = (output_.data).cpu().numpy()[j]
+        vis = meta_.cpu().numpy()[j][5:]
         for t in range(ref.J):
           f.write('{} {} {} '.format(pred[t * 3], pred[t * 3 + 1], pred[t * 3 + 2]))
         f.write('\n')
@@ -144,14 +153,14 @@ def dial_step(args, split, epoch, (loader, len_loader), model, optimizer = None,
             f.write('{} 0 0 '.format(vis[t]))
           f.write('\n')
 
-    mpjpe_this = accuracy(output.data, target, meta)
-    mpjpe_r_this = accuracy_dis(output.data, target, meta)
-    shapeLoss = shapeConsistency(output.data, meta, nViews, M, split = split)
+    mpjpe_this = accuracy(output_.data, target_, meta_)
+    mpjpe_r_this = accuracy_dis(output_.data, target_, meta_)
+    shapeLoss = shapeConsistency(output_.data, meta_, nViews, M, split = split)
 
-    losses.update(source_loss_value + target_loss_value, input.size(0))
-    shapeLosses.update(shapeLoss, input.size(0))
-    mpjpe.update(mpjpe_this, input.size(0))
-    mpjpe_r.update(mpjpe_r_this, input.size(0))
+    losses.update(source_loss_value + target_loss_value, input_.size(0))
+    shapeLosses.update(shapeLoss, input_.size(0))
+    mpjpe.update(mpjpe_this, input_.size(0))
+    mpjpe_r.update(mpjpe_r_this, input_.size(0))
     
     
     Bar.suffix = '{split:10}: [{0:2}][{1:3}/{2:3}] | Total: {total:} | ETA: {eta:} | Loss {loss.avg:.6f} | shapeLoss {shapeLoss.avg:.6f} | AE {mpjpe.avg:.6f} | ShapeDis {mpjpe_r.avg:.6f}'.format(epoch, i, len_loader, total=bar.elapsed_td, eta=bar.eta_td, loss=losses, mpjpe=mpjpe, split = split, shapeLoss = shapeLosses, mpjpe_r = mpjpe_r)
