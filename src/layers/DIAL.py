b9955ac16eda51c36b4cac4d6c590e1969f971a9
Levi O. Vasconcelos <leviovasconcelos@gmail.com>
2019-02-04 18:14:26 +0100

    reverting: removing affine transfomations from batch norms in DIAL layer

--- src/layers/DIAL.py
+++ src/layers/DIAL.py
@@ -6,16 +6,16 @@ class DomainAdaptationLayer(nn.Module):
         super(DomainAdaptationLayer, self).__init__()
         
         self.bn_source = nn.BatchNorm2d(planes)
-        nn.init.constant(self.bn_source.weight, 1)
-        nn.init.constant(self.bn_source.bias, 0)
-        self.bn_source.weight.requires_grad = False
-        self.bn_source.bias.requires_grad = False
+        #nn.init.constant(self.bn_source.weight, 1)
+        #nn.init.constant(self.bn_source.bias, 0)
+        #self.bn_source.weight.requires_grad = False
+        #Sself.bn_source.bias.requires_grad = False
         
         self.bn_target = nn.BatchNorm2d(planes)
-        nn.init.constant(self.bn_target.weight, 1)
-        nn.init.constant(self.bn_target.bias, 0)
-        self.bn_target.weight.requires_grad = False
-        self.bn_target.bias.requires_grad = False
+        #nn.init.constant(self.bn_target.weight, 1)
+        #nn.init.constant(self.bn_target.bias, 0)
+        #self.bn_target.weight.requires_grad = False
+        #self.bn_target.bias.requires_grad = False
         
         self.weight = nn.parameter.Parameter(torch.Tensor(planes))
         self.bias = nn.parameter.Parameter(torch.Tensor(planes))
