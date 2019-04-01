6497ab9623c8e16d9cd5ade6c358884ee5b29399
Levi O. Vasconcelos <leviovasconcelos@gmail.com>
2019-04-01 16:04:05 +0200

    adding tensorboard visualizations

--- src/main_vis.py
+++ src/main_vis.py
@@ -180,8 +180,17 @@ def main():
                   if args.shapeWeight > ref.eps and epoch % args.intervalUpdateM == 0:
                         M = stepLatent(trainTarget_loader, model, M, Y, nViews = args.nViews, lamb = args.lamb, mu = args.mu, S = args.sampleSource, call_count=call_count, dial=DIAL)
                         call_count += 1
-                  valSource_mpjpe, valSource_loss, valSource_unSuploss = validate(args, 'Source', valSource_loader, model, None, epoch)
-                  valTarget_mpjpe, valTarget_loss, valTarget_unSuploss = validate(args, 'Target', valTarget_loader, model, None, epoch)
+                  if epoch % 2 == 0:
+                        valTarget_mpjpe, valTarget_loss, valTarget_unSuploss = validate(args, 'Target', valTarget_loader, model, None, epoch)
+                        logger.add_scalar('val/target-accuracy', valTarget_mpjpe, epoch)
+                        logger.add_scalar('val/target-regr-loss', valTarget_loss, epoch)
+                        logger.add_scalar('val/target-prior-loss', valTarget_unSuploss, epoch)
+                  if epoch % 5 == 0:
+                        valSource_mpjpe, valSource_loss, valSource_unSuploss = validate(args, 'Source', valSource_loader, model, None, epoch)
+                        logger.add_scalar('val/source-accuracy', valTarget_mpjpe, epoch)
+                        logger.add_scalar('val/source-regr-loss', valTarget_loss, epoch)
+                        logger.add_scalar('val/source-prior-loss', valTarget_unSuploss, epoch)
+                        
             else:
                   train_priors(args, [trainTarget_loader], model, prior_loss, args.batch_norm, logger, optimizer, epoch-1, threshold = args.threshold)
                   
